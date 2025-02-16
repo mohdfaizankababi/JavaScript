2cf870668ac99bfb519048e6ac86ea4cc4cab5b9
mohdfaizankababi <mohdfaizankababi@gmail.com>
2025-02-16 10:31:04 +0000

    basics

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -15,3 +15,21 @@ let isLoggedIn = 1;
 
 let booleanIsLoggedIn = Boolean(isLoggedIn);
 console.log(typeof booleanIsLoggedIn)
+
+//**************************OPERATIONS******************* */
+
+
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+
+let str1 ="Hello"
+let str2 = " Faizan"
+let str3 = str1 + str2
+console.log(str3);
+
+
+// ===
+
+
+console.log("2"=== 2);//strict check
